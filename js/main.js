37bd8d759d5b3ae2682554b864f2a43a5806ae14
MaksimSchcipanov <73181650+MaksimSchcipanov@users.noreply.github.com>
2020-10-20 21:59:32 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/**************************************/
+/* Vendor JavaScript files supervisor */
+/**************************************/
+
+/* Third party */
+
+////= ../../bower_components/headjs/dist/1.0.0/head.js
+////= ../../bower_components/jquery/dist/jquery.js
+////= ../../bower_components/bootstrap/dist/js/bootstrap.js
+////= ../../bower_components/modernizr/modernizr.js
+////= ../../bower_components/css_browser_selector/css_browser_selector.js
+/**************************************/
+/* Custom JavaScript files supervisor */
+/**************************************/
+
+//$(document).ready(function() {
+//
+//    /* Custom */
+//
+//
+//});
